740f94c9a9ec50e9e3cb31bd317e05bebdb93470
TalitaK564 <talita.kauane@escolar.ifrn.edu.br>
2026-06-29 21:24:56 +0000

    fix: adição da função load_logged_in_user (auth.py) e base.html

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -38,7 +38,6 @@ def register():
 
     return render_template('auth/register.html')
 
-flaskr/auth.py
 @bp.route('/login', methods=('GET', 'POST'))
 def login():
     if request.method == 'POST':
@@ -63,6 +62,22 @@ def login():
         flash(error)
 
     return render_template('auth/login.html')
+
+@bp.before_app_request
+def load_logged_in_user():
+    user_id = session.get('user_id')
+
+    if user_id is None:
+        g.user = None
+    else:
+        g.user = get_db().execute(
+            'SELECT * FROM user WHERE id = ?', (user_id,)
+        ).fetchone()
+
+@bp.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for('index'))
 
 def login_required(view):
     @functools.wraps(view)
