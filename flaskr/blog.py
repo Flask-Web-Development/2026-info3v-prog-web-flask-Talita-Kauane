07eb93005fc0451040f6b91625862516221b702d
TalitaK564 <talita.kauane@escolar.ifrn.edu.br>
2026-06-29 23:51:08 +0000

    fix: identação errada em blog.py

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -8,6 +8,7 @@ from flaskr.db import get_db
 
 bp = Blueprint('blog', __name__)
 
+
 @bp.route('/')
 def index():
     db = get_db()
@@ -17,6 +18,7 @@ def index():
         ' ORDER BY created DESC'
     ).fetchall()
     return render_template('blog/index.html', posts=posts)
+
 
 @bp.route('/create', methods=('GET', 'POST'))
 @login_required
@@ -43,33 +45,23 @@ def create():
 
     return render_template('blog/create.html')
 
+
 def get_post(id, check_author=True):
+    post = get_db().execute(
+        'SELECT p.id, title, body, created, author_id, username'
+        ' FROM post p JOIN user u ON p.author_id = u.id'
+        ' WHERE p.id = ?',
+        (id,)
+    ).fetchone()
 
-post = get_db().execute(
+    if post is None:
+        abort(404, f"Post id {id} doesn't exist.")
 
-'SELECT p.id, title, body, created, author_id, username'
+    if check_author and post['author_id'] != g.user['id']:
+        abort(403)
 
-' FROM post p JOIN user u ON p.author_id = u.id'
+    return post
 
-' WHERE p.id = ?',
-
-(id,)
-
-).fetchone()
-
-
-
-if post is None:
-
-abort(404, f"Post id {id} doesn't exist.")
-
-
-
-if check_author and post['author_id'] != g.user['id']:
-
-abort(403)
-
-return post 
 
 @bp.route('/<int:id>/update', methods=('GET', 'POST'))
 @login_required
@@ -97,3 +89,13 @@ def update(id):
             return redirect(url_for('blog.index'))
 
     return render_template('blog/update.html', post=post)
+
+
+@bp.route('/<int:id>/delete', methods=('POST',))
+@login_required
+def delete(id):
+    get_post(id)
+    db = get_db()
+    db.execute('DELETE FROM post WHERE id = ?', (id,))
+    db.commit()
+    return redirect(url_for('blog.index'))
