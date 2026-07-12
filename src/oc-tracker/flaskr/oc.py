5a7f3e23d9c02b2c9ba70d62c947e4dc7bf75d65
TalitaK564 <talita.kauane@escolar.ifrn.edu.br>
2026-07-12 14:27:23 +0000

    feat: adicionando função create para criação de personagens #2

--- src/oc-tracker/flaskr/oc.py
+++ src/oc-tracker/flaskr/oc.py
@@ -18,3 +18,38 @@ def index():
         ' ORDER BY p.criado DESC'
     ).fetchall()
     return render_template('blog/index.html', posts=posts)
+
+@bp.route('/create', methods=('GET', 'POST'))
+@login_required
+def create():
+    if request.method == 'POST':
+        nome = request.form['nome']
+        especie = request.form['especie']
+        biografia = request.form['biografia']
+        alinhamento = request.form.get('alinhamento')  
+        pronomes = request.form.get('pronomes')        
+        ocupacao = request.form.get('ocupacao')        
+        imagem_url = request.form.get('imagem_url')    
+        
+        error = None
+
+        if not nome:
+            error = 'O nome do personagem é obrigatório.'
+        elif not especie:
+            error = 'A espécie/raça é obrigatória.'
+        elif not biografia:
+            error = 'A biografia ou história é obrigatória.'
+
+        if error is not None:
+            flash(error)
+        else:
+            db = get_db()
+            db.execute(
+                'INSERT INTO personagem (user_id, nome, especie, alinhamento, pronomes, ocupacao, biografia, imagem_url)'
+                ' VALUES (?, ?, ?, ?, ?, ?, ?, ?)',
+                (g.user['id'], nome, especie, alinhamento, pronomes, ocupacao, biografia, imagem_url)
+            )
+            db.commit()
+            return redirect(url_for('blog.index'))
+
+    return render_template('blog/create.html')
