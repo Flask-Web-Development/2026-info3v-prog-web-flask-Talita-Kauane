cf0a1276f20e6030726723f21f10f66323c53cd6
TalitaK564 <talita.kauane@escolar.ifrn.edu.br>
2026-07-12 14:17:11 +0000

    feat: implementação da página principal do app #2

--- src/oc-tracker/flaskr/__init__.py
+++ src/oc-tracker/flaskr/__init__.py
@@ -32,8 +32,8 @@ def create_app(test_config=None):
     from . import auth
     app.register_blueprint(auth.bp)
 
-    from . import blog
-    app.register_blueprint(blog.bp)
+    from . import oc
+    app.register_blueprint(oc.bp)
     app.add_url_rule('/', endpoint='index')
 
     return app
--- src/oc-tracker/flaskr/oc.py
+++ src/oc-tracker/flaskr/oc.py
@@ -7,3 +7,14 @@ from flaskr.auth import login_required
 from flaskr.db import get_db
 
 bp = Blueprint('blog', __name__)
+
+@bp.route('/')
+def index():
+    db = get_db()
+    # Mudamos para buscar os campos reais da sua tabela 'personagem'
+    posts = db.execute(
+        'SELECT p.id, p.nome AS title, p.biografia AS body, p.criado AS created, p.user_id AS author_id, u.username'
+        ' FROM personagem p JOIN user u ON p.user_id = u.id'
+        ' ORDER BY p.criado DESC'
+    ).fetchall()
+    return render_template('blog/index.html', posts=posts)
